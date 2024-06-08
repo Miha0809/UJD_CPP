26cc1d3984d9aa06ce2afa927107f1b8465a4452
Miha0809 <miha080104@gmail.com>
2024-06-08 23:22:46 +0200

    Task3

--- Lab5/structures.cpp
+++ Lab5/structures.cpp
@@ -74,12 +74,80 @@ void task2(void) {
   cout << "The perimeter of the triangle is: " << perimeter << endl;
 }
 
+// Task3
+
+struct Data {
+public:
+  int integer;
+  int *intPointer;
+  char *charPointer;
+  char *charArrayPointer;
+  std::string str;
+};
+
+void modifyDataByValue(Data data) {
+  data.integer *= 2;
+  *(data.intPointer) *= 2;
+  *(data.charPointer) = 'X';
+  *(data.charArrayPointer + 1) = 'Y';
+  data.str = "Modified by value";
+}
+
+void modifyDataByPointer(Data *data) {
+  data->integer *= 3;
+  *(data->intPointer) *= 3;
+  *(data->charPointer) = 'Z';
+  *(data->charArrayPointer + 2) = 'W';
+  data->str = "Modified by pointer";
+}
+
+void task3(void) {
+  Data data;
+  int intValue = 10;
+  char charValue = 'A';
+  char charArray[] = "Hello";
+
+  data.integer = 5;
+  data.intPointer = &intValue;
+  data.charPointer = &charValue;
+  data.charArrayPointer = charArray;
+  data.str = "Original";
+
+  cout << "Before calling functions:\n";
+  cout << "Integer: " << data.integer << endl;
+  cout << "Integer pointed by intPointer: " << *(data.intPointer) << endl;
+  cout << "Character pointed by charPointer: " << *(data.charPointer) << endl;
+  cout << "Character in charArrayPointer: " << data.charArrayPointer[1] << endl;
+  cout << "String: " << data.str << endl << endl;
+
+  modifyDataByValue(data);
+
+  cout << "After calling modifyDataByValue:\n";
+  cout << "Integer: " << data.integer << endl;
+  cout << "Integer pointed by intPointer: " << *(data.intPointer) << endl;
+  cout << "Character pointed by charPointer: " << *(data.charPointer) << endl;
+  cout << "Character in charArrayPointer: " << data.charArrayPointer[1] << endl;
+  cout << "String: " << data.str << endl << endl;
+
+  modifyDataByPointer(&data);
+
+  cout << "After calling modifyDataByPointer:\n";
+  cout << "Integer: " << data.integer << endl;
+  cout << "Integer pointed by intPointer: " << *(data.intPointer) << endl;
+  cout << "Character pointed by charPointer: " << *(data.charPointer) << endl;
+  cout << "Character in charArrayPointer: " << data.charArrayPointer[1] << endl;
+  cout << "String: " << data.str << endl;
+}
+
 int main(void) {
   cout << "Task1!" << endl;
   task1();
 
   cout << "Task2!" << endl;
   task2();
+
+  cout << "Task3!" << endl;
+  task3();
 
   return 0;
 }
