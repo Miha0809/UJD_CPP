d121438c342b59b75280953538454b9d04655471
Miha0809 <miha080104@gmail.com>
2024-06-16 19:12:53 +0200

    Task6a

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -65,6 +65,37 @@ void task6(void) {
        << endl;
 }
 
+struct IPAddress {
+  unsigned char bytes[4];
+};
+IPAddress calculateNetworkAddress(const IPAddress &ip,
+                                  const IPAddress &subnetMask) {
+  IPAddress networkAddress;
+
+  for (int i = 0; i < 4; ++i) {
+    networkAddress.bytes[i] = ip.bytes[i] & subnetMask.bytes[i];
+  }
+
+  return networkAddress;
+}
+void printIPAddress(const IPAddress &ip) {
+  cout << (int)ip.bytes[0] << "." << (int)ip.bytes[1] << "." << (int)ip.bytes[2]
+       << "." << (int)ip.bytes[3] << endl;
+}
+void task6a(void) {
+  IPAddress ip = {{192, 168, 1, 10}};
+  IPAddress subnetMask = {{255, 255, 255, 0}};
+
+  IPAddress networkAddress = calculateNetworkAddress(ip, subnetMask);
+
+  cout << "Adres IP: ";
+  printIPAddress(ip);
+  cout << "Maska sieci: ";
+  printIPAddress(subnetMask);
+  cout << "Adres sieci: ";
+  printIPAddress(networkAddress);
+}
+
 int main(void) {
   cout << endl << "Task4!" << endl;
   task4();
@@ -74,6 +105,9 @@ int main(void) {
 
   cout << endl << "Task6!" << endl;
   task6();
+
+  cout << endl << "Task6a!" << endl;
+  task6a();
 
   return 0;
 }
